1b9cfb5942b306fb3bf919971e8f04016d986c2f
thomasgreen <tomgreen135@gmail.com>
2017-02-23 11:18:23 +0000

    Development of human class

--- AIProject/src/Human.java
+++ AIProject/src/Human.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //controls to be added here, not sure how to do that yet
 public class Human extends Player {
 	//human class used for controlling the game 
+	
+	private List<Checker> playerCheckers;
+	private String CheckerColour;
+	
+	public Human(String checkerColour){
+		
+		playerCheckers = new ArrayList<>();
+		this.CheckerColour = checkerColour;
+	}
+
+	public List<Checker> getPlayerCheckers() {
+		return playerCheckers;
+	}
+
+	public void setPlayerCheckers(List<Checker> playerCheckers) {
+		this.playerCheckers = playerCheckers;
+	}
+
+	public String getCheckerColour() {
+		return CheckerColour;
+	}
+
+	public void setCheckerColour(String checkerColour) {
+		CheckerColour = checkerColour;
+	}
+	
+	
 }
